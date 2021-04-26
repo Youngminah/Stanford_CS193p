c5460bcce1e416974fd7b83b7bf384214f44f217
Youngminah <bluebelsmudi@gmail.com>
2021-04-26 19:48:44 +0900

    Add : 1번째 Memorize 프로젝트

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by meng on 2021/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
